0e801b17e89a2a1b2d8f66e943cee985ea13dce4
Gustavocgsg <gustavocgsg@gmail.com>
2020-07-19 21:30:20 -0300

    pagina 404

---
+++ advogados-associados-frontend/pages/404.jsx
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+
+export default function erro404() {
+    return (
+        <section>
+            <section className="hero is-large is-bold img-erro404 bg-cover">
+                <div className="hero-body">
+                <div className="container has-text-centered">
+                    <h1 className="title is-size-3 is-size-5-mobile pb-2 has-text-primary">
+                        Erro 404 - guga
+                    </h1>
+                </div>
+                </div>
+            </section>
+        </section>
+    )
+}
